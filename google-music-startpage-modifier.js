138b19b88f0f63ab6aef633f45b2c8c1dcc4964f
Thomas Vöcking <thomas@senselessweb.com>
2013-01-16 04:03:47 -0800

    Create google-music-startpage-modifier.js

---
+++ google-music-startpage-modifier.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Google music startpage modifier
+// @version    1
+// @description  Replaces the links in the black bar on google's homepage by something more useful
+// @match      *.play.google.com/music/listen#start
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
+// ==/UserScript==
+$(function() 
+  {
+    console.log('Running Google music start page modifier');
+  }
+);
